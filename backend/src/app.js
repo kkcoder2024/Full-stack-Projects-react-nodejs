056eaaf1256932c541175c0a22178296f8b4708f
Koushik Karmakar <koushik9339mail@gmail.com>
2025-12-19 17:48:50 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [
+      "http://localhost:5173",
+      "https://productivity-hub-21a1.onrender.com"
+    ],
     credentials: true,
   })
 );
